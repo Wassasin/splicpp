c4065f4bcd8e190db2bfd78fde35d3d42494c163
Wouter Geraedts <w.geraedts@onii.org>
2012-03-25 23:12:26 +0200

    Added array type

---
+++ src/typing/types/sl_type_array.cpp
@@ -0,0 +1,27 @@
+#include "sl_type_array.hpp"
+
+namespace splicpp
+{	
+	sl_type::sl_type_type sl_type_array::type() const
+	{
+		return t_array;
+	}
+	
+	void sl_type_array::print(std::ostream& s) const
+	{
+		s << '[';
+		t->print(s);
+		s << ']';
+	}
+	
+	std::vector<std::shared_ptr<sl_type_unbound>> sl_type_array::fv() const
+	{
+		return t->fv();
+	}
+	
+	//substitution sl_type_array::unify(const std::shared_ptr<sl_type> t, typecontext& c) const; //TODO
+	std::shared_ptr<sl_type> sl_type_array::apply(const substitution& s) const
+	{
+		return std::shared_ptr<sl_type>(new sl_type_array(t->apply(s)));
+	}
+}
---
+++ src/typing/types/sl_type_array.hpp
@@ -0,0 +1,25 @@
+#ifndef SL_TYPE_ARRAY_H
+#define SL_TYPE_ARRAY_H
+
+#include "sl_type.hpp"
+
+namespace splicpp
+{	
+	class sl_type_array : public sl_type
+	{
+		std::shared_ptr<sl_type> t;
+	public:
+		sl_type_array(std::shared_ptr<sl_type> t)
+		: t(t)
+		{}
+		
+		virtual sl_type_type type() const;
+		virtual void print(std::ostream& s) const;
+		
+		virtual std::vector<std::shared_ptr<sl_type_unbound>> fv() const;
+		//virtual substitution unify(const std::shared_ptr<sl_type> t, typecontext& c) const; //TODO
+		virtual std::shared_ptr<sl_type> apply(const substitution& s) const;
+	};
+}
+
+#endif
