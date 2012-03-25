ce0190aa240a59da737e690368fcc334fb9e03c9
Wouter Geraedts <w.geraedts@onii.org>
2012-03-25 15:02:49 +0200

    Added typecontext

---
+++ src/typing/typecontext.cpp
@@ -0,0 +1,31 @@
+#include "typecontext.hpp"
+
+#include <stdexcept>
+
+#include "types/sl_type.hpp"
+#include "types/sl_type_unbound.hpp"
+
+namespace splicpp
+{
+	void typecontext::register_type(const sid i, const std::shared_ptr<sl_type> t)
+	{
+		if(i >= types.size())
+			types.resize(i+1);
+		
+		types[i] = t;
+	}
+	
+	std::shared_ptr<sl_type> typecontext::operator[](const sid i) const
+	{
+		const auto t = types.at(i);
+		if(!t)
+			throw std::logic_error("Type not inserted into context");
+		
+		return t.get();
+	}
+	
+	std::shared_ptr<sl_type_unbound> typecontext::create_fresh()
+	{
+		return std::shared_ptr<sl_type_unbound>(new sl_type_unbound(ft_count++));
+	}
+}
---
+++ src/typing/typecontext.hpp
@@ -0,0 +1,32 @@
+#ifndef TYPECONTEXT_H
+#define TYPECONTEXT_H
+
+#include <vector>
+#include <memory>
+#include <boost/optional.hpp>
+
+#include "../common/typedefs.hpp"
+
+namespace splicpp
+{
+	class sl_type;
+	class sl_type_unbound;
+
+	class typecontext
+	{
+		std::vector<boost::optional<std::shared_ptr<sl_type>>> types;
+		uint ft_count;
+		
+	public:
+		typecontext()
+		: types()
+		, ft_count()
+		{}
+		
+		void register_type(const sid i, const std::shared_ptr<sl_type> t);
+		std::shared_ptr<sl_type> operator[](const sid i) const;
+		std::shared_ptr<sl_type_unbound> create_fresh();
+	};
+}
+
+#endif
