4894905f6eb532fd48284d80f8d15189ad85d2c9
Wouter Geraedts <w.geraedts@onii.org>
2012-04-24 17:31:31 +0200

    Prepared sl_polytype_exists initialization

--- src/typing/symboltable.cpp
+++ src/typing/symboltable.cpp
@@ -81,13 +81,13 @@ namespace splicpp
 		ltypecontext c(global);
 		substitution s;
 		
-		std::map<sid, cs_ptr<sl_type_unbound>> init_types;
+		std::map<sid, cs_ptr<sl_polytype_exists>> init_types;
 		for(size_t i = 0; i < index.size(); i++)
-			if(index[i].t != symbolref::t_type && index[i].t != symbolref::t_arg && index[i].t != symbolref::t_local_var)
+			if(index[i].t == symbolref::t_var || index[i].t == symbolref::t_fun)
 			{
-				const cs_ptr<sl_type_unbound> t = c.create_fresh();
+				const cs_ptr<sl_polytype_exists> t(new sl_polytype_exists());
 				init_types[i] = t;
-				c.register_type(i, sl_polytype::qualify(c, t));
+				c.register_type(i, std::static_pointer_cast<const sl_polytype>(t));
 			}
 		
 		print(c, std::cout << std::endl << "Typecontext initial: " << std::endl);
@@ -95,9 +95,7 @@ namespace splicpp
 		for(const sid i : select_all(symbolref::symbolreftype::t_construct))
 		{
 			c = c.apply(s);
-			cs_ptr<sl_type_unbound> t = init_types[i];
 			s = conss[index[i].i]->declare_type(c).composite(s);
-			//s = init_types[i]->apply(s)->unify(c[i]->apply(c, s)->unbind_maintain()).composite(s);
 		}
 		
 		//s.print(std::cout << std::endl << "Substitutions: " << std::endl);
@@ -105,18 +103,13 @@ namespace splicpp
 		for(const sid i : select_all(symbolref::symbolreftype::t_var))
 		{
 			c = c.apply(s);
-			cs_ptr<sl_type_unbound> t = init_types[i];
 			s = vars[index[i].i]->declare_type(c).composite(s);
-			//s = init_types[i]->apply(s)->unify(c[i]->apply(c, s)->unbind_maintain()).composite(s);
 		}
 		
 		for(const sid i : select_all(symbolref::symbolreftype::t_fun))
 		{
 			c = c.apply(s);
 			s = funs[index[i].i]->declare_type(c).composite(s);
-			//c = c.apply(s);
-			
-			//s = init_types[i]->apply(s)->unify(c[i]->apply(c, s)->unbind_maintain()).composite(s);
 		}
 		
 		//for(const sid i : select_all(symbolref::symbolreftype::t_local_var))
