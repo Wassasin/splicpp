a681381b264a1335f132e2b6fb0e554f4d629022
Wouter Geraedts <w.geraedts@onii.org>
2012-04-29 16:08:30 +0200

    Fixed printing-bug in sloc.cpp with unitialized line_start

--- src/common/sloc.cpp
+++ src/common/sloc.cpp
@@ -12,12 +12,12 @@ namespace splicpp
 
 	void sloc::print(std::string str, std::ostream& s) const
 	{
-		size_t line_start;
+		size_t line_start = 0;
 		for(size_t i = 0; i < pos; i++)
 			if(str[i] == '\n')
-				line_start = i;
+				line_start = i+1;
 		
-		for(size_t i = line_start+1; i < str.length(); i++)
+		for(size_t i = line_start; i < str.length(); i++)
 		{
 			if(str[i] == '\n')
 				break;
@@ -27,7 +27,7 @@ namespace splicpp
 		
 		s << std::endl;
 		
-		for(size_t i = line_start+1; i < pos; i++)
+		for(size_t i = line_start; i < pos; i++)
 		{	
 			if(str[i] == '\t')
 				s << '\t';
