9557824ecfea63eabbb359533c31620910ebf661
Wouter Geraedts <w.geraedts@onii.org>
2012-05-08 12:56:16 +0200

    Allowing ircontext to be copied

--- src/ir/ircontext.cpp
+++ src/ir/ircontext.cpp
@@ -4,12 +4,12 @@ namespace splicpp
 {
 	ir_label ircontext::create_label()
 	{
-		return lcount++;
+		return (*lcount)++;
 	}
 	
 	ir_temp ircontext::create_temporary()
 	{
-		return tcount++;
+		return (*tcount)++;
 	}
 
 	ir_label ircontext::fetch_label(const sid i) const
--- src/ir/ircontext.hpp
+++ src/ir/ircontext.hpp
@@ -9,22 +9,22 @@ namespace splicpp
 {
 	class ircontext
 	{
-		ir_label lcount;
-		ir_temp tcount;
+		s_ptr<ir_label> lcount;
+		s_ptr<ir_temp> tcount;
 		
 		std::map<sid, ir_label> memmap;
 	public:
 		const ir_temp heap_reg;
-		const ir_temp stack_reg;
+		const ir_temp frame_reg;
 		
 		const ir_label l_nil;
 		
 		ircontext()
-		: lcount(0)
-		, tcount(0)
+		: lcount(new ir_label(0))
+		, tcount(new ir_temp(0))
 		, memmap()
 		, heap_reg(create_temporary())
-		, stack_reg(create_temporary())
+		, frame_reg(create_temporary())
 		, l_nil(create_label())
 		{}
 		
