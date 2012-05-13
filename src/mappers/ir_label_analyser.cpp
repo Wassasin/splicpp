8c4ce97c5ef75fef31e60bb5e1837781a1b84339
Wouter Geraedts <w.geraedts@onii.org>
2012-05-13 23:07:34 +0200

    Implemented IR label analysis

---
+++ src/mappers/ir_label_analyser.cpp
@@ -0,0 +1,53 @@
+#include "ir_label_analyser.hpp"
+
+#include <stdexcept>
+
+#include "../ir/ir_stmt_cjump.hpp"
+#include "../ir/ir_stmt_jump.hpp"
+#include "../ir/ir_stmt_label.hpp"
+#include "../ir/ir_stmt_move.hpp"
+#include "../ir/ir_stmt_seq.hpp"
+
+namespace splicpp
+{
+	std::map<ir_label, size_t> ir_label_analyser::analyse(const std::list<s_ptr<const ir_stmt>>& stmts)
+	{
+		ir_label_analyser a;
+		
+		for(const auto stmt : stmts)
+		{
+			stmt->map(a);
+			a.i++;
+		}
+		
+		return a.index;
+	}
+
+	void ir_label_analyser::map(const s_ptr<const ir_stmt_cjump>)
+	{
+		//Do nothing
+	}
+	
+	void ir_label_analyser::map(const s_ptr<const ir_stmt_jump>)
+	{
+		//Do nothing
+	}
+	
+	void ir_label_analyser::map(const s_ptr<const ir_stmt_label> x)
+	{
+		if(index.find(x->l) != index.end())
+			throw std::logic_error("Multiple occurences of label found");
+		
+		index[x->l] = i;
+	}
+	
+	void ir_label_analyser::map(const s_ptr<const ir_stmt_move>)
+	{
+		//Do nothing
+	}
+	
+	void ir_label_analyser::map(const s_ptr<const ir_stmt_seq>)
+	{
+		//Do nothing
+	}
+}
---
+++ src/mappers/ir_label_analyser.hpp
@@ -0,0 +1,35 @@
+#ifndef IR_LABEL_ANALYSER_H
+#define IR_LABEL_ANALYSER_H
+
+#include <map>
+#include <list>
+
+#include "../common/typedefs.hpp"
+
+#include "generic/ir_stmt_mapper.hpp"
+
+namespace splicpp
+{
+	class ir_stmt;
+	
+	class ir_label_analyser : public ir_stmt_mapper
+	{
+		size_t i;
+		std::map<ir_label, size_t> index;
+		
+		ir_label_analyser()
+		: i(0)
+		, index()
+		{}
+	public:
+		static std::map<ir_label, size_t> analyse(const std::list<s_ptr<const ir_stmt>>& stmts);
+		
+		virtual void map(const s_ptr<const ir_stmt_cjump> x);
+		virtual void map(const s_ptr<const ir_stmt_jump> x);
+		virtual void map(const s_ptr<const ir_stmt_label> x);
+		virtual void map(const s_ptr<const ir_stmt_move> x);
+		virtual void map(const s_ptr<const ir_stmt_seq> x);
+	};
+}
+
+#endif
