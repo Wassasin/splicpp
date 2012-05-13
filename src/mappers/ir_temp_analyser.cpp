3c85d9bcce54909253e5bcb9b57b7a586c94b6bb
Wouter Geraedts <w.geraedts@onii.org>
2012-05-14 01:21:14 +0200

    Implemented usage analysis of temporaries

---
+++ src/mappers/ir_temp_analyser.cpp
@@ -0,0 +1,113 @@
+#include "ir_temp_analyser.hpp"
+
+#include <stdexcept>
+
+#include "../common/generic.hpp"
+
+#include "../ir/ir_exp_binop.hpp"
+#include "../ir/ir_exp_const.hpp"
+#include "../ir/ir_exp_eseq.hpp"
+#include "../ir/ir_exp_mem.hpp"
+#include "../ir/ir_exp_name.hpp"
+#include "../ir/ir_exp_temp.hpp"
+
+#include "../ir/ir_stmt_cjump.hpp"
+#include "../ir/ir_stmt_jump.hpp"
+#include "../ir/ir_stmt_label.hpp"
+#include "../ir/ir_stmt_move.hpp"
+#include "../ir/ir_stmt_seq.hpp"
+
+namespace splicpp
+{
+	void ir_temp_analyser::analyse(const s_ptr<const ir_exp> x)
+	{
+		x->map(*this);
+	}
+	
+	void ir_temp_analyser::analyse(const s_ptr<const ir_stmt> x)
+	{
+		x->map(*this);
+	}
+	
+	std::vector<ir_temp_analyser::usage> ir_temp_analyser::analyse(const std::vector<s_ptr<const ir_stmt>>& stmts)
+	{
+		std::vector<usage> result;
+		result.reserve(stmts.size());
+	
+		for(const auto stmt : stmts)
+		{
+			ir_temp_analyser a;
+			a.analyse(stmt);
+			result.push_back(a.u);
+		}
+		
+		return result;
+	}
+
+	//Inherited from ir_exp_transformer
+	void ir_temp_analyser::map(const s_ptr<const ir_exp_binop> x)
+	{
+		analyse(x->e_left);
+		analyse(x->e_right);
+	}
+	
+	void ir_temp_analyser::map(const s_ptr<const ir_exp_const>)
+	{
+		//Do nothing
+	}
+	
+	void ir_temp_analyser::map(const s_ptr<const ir_exp_eseq>)
+	{
+		throw std::logic_error("ir_exp_eseq is not allowed in this stage; run the ir_desequencer first");
+	}
+	
+	void ir_temp_analyser::map(const s_ptr<const ir_exp_mem> x)
+	{
+		analyse(x->e);
+	}
+	
+	void ir_temp_analyser::map(const s_ptr<const ir_exp_name>)
+	{
+		//Do nothing
+	}
+	
+	void ir_temp_analyser::map(const s_ptr<const ir_exp_temp> x)
+	{
+		if(!is_in<ir_temp>(x->t, u.used))
+			u.used.push_back(x->t);
+	}
+
+	//Inherited from ir_stmt_transformer
+	void ir_temp_analyser::map(const s_ptr<const ir_stmt_cjump> x)
+	{
+		analyse(x->e_left);
+		analyse(x->e_right);
+	}
+	
+	void ir_temp_analyser::map(const s_ptr<const ir_stmt_jump> x)
+	{
+		analyse(x->e);
+	}
+	
+	void ir_temp_analyser::map(const s_ptr<const ir_stmt_label>)
+	{
+		//Do nothing
+	}
+	
+	void ir_temp_analyser::map(const s_ptr<const ir_stmt_move> x)
+	{
+		const s_ptr<const ir_exp_temp> e_left_temp(std::dynamic_pointer_cast<const ir_exp_temp>(x->e_left));
+		
+		if(e_left_temp == nullptr)
+			analyse(x->e_left); //Does not set a temp, thus check if a temp is used to set anything else
+		else
+			u.defined = e_left_temp->t;
+		
+		analyse(x->e_right);
+	}
+	
+	void ir_temp_analyser::map(const s_ptr<const ir_stmt_seq>)
+	{
+		throw std::logic_error("ir_stmt_seq is not allowed in this stage; run the ir_desequencer first");
+	}
+}
---
+++ src/mappers/ir_temp_analyser.hpp
@@ -0,0 +1,59 @@
+#ifndef IR_TEMP_ANALYSER_H
+#define IR_TEMP_ANALYSER_H
+
+#include <vector>
+#include <boost/optional.hpp>
+
+#include "../common/typedefs.hpp"
+
+#include "generic/ir_exp_mapper.hpp"
+#include "generic/ir_stmt_mapper.hpp"
+
+namespace splicpp
+{
+	class ir_stmt;
+	class ir_exp;
+
+	class ir_temp_analyser : public ir_exp_mapper, public ir_stmt_mapper
+	{
+	public:
+		struct usage
+		{
+			boost::optional<ir_temp> defined;
+			std::vector<ir_temp> used;
+			
+			usage()
+			: defined()
+			, used()
+			{}
+		};
+		
+	private:
+		usage u;
+		
+		ir_temp_analyser()
+		: u()
+		{}
+		
+		void analyse(const s_ptr<const ir_exp> x);
+		void analyse(const s_ptr<const ir_stmt> x);
+		
+	public:
+		static std::vector<usage> analyse(const std::vector<s_ptr<const ir_stmt>>& stmts);
+	
+		virtual void map(const s_ptr<const ir_exp_binop> x);
+		virtual void map(const s_ptr<const ir_exp_const> x);
+		virtual void map(const s_ptr<const ir_exp_eseq> x);
+		virtual void map(const s_ptr<const ir_exp_mem> x);
+		virtual void map(const s_ptr<const ir_exp_name> x);
+		virtual void map(const s_ptr<const ir_exp_temp> x);
+		
+		virtual void map(const s_ptr<const ir_stmt_cjump> x);
+		virtual void map(const s_ptr<const ir_stmt_jump> x);
+		virtual void map(const s_ptr<const ir_stmt_label> x);
+		virtual void map(const s_ptr<const ir_stmt_move> x);
+		virtual void map(const s_ptr<const ir_stmt_seq> x);
+	};
+}
+
+#endif
